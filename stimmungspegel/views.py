2ebb9bf6118900ca960981613638f972543a4b84
techkruste <sven.kipry@smail.th-koeln.de>
2017-05-02 13:53:42 +0200

    About page

--- stimmungspegel/views.py
+++ stimmungspegel/views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'stimmungspegel/index.html')
 
-
 def search(request):
     return render(request, 'stimmungspegel/search.html')
 
 def add(request):
     return render(request, 'stimmungspegel/add.html')
+
+def about(request):
+    return render(request, 'stimmungspegel/about.html')
